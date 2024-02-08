617a8ea53ac847e0124ddc290b87b494cf88cd8d
KidusGebrgiorgis <84538476+KidusGebrgiorgis@users.noreply.github.com>
2024-02-07 20:23:37 -0500

    Planning phase

--- Resume_builder/To-do-list.py
+++ Resume_builder/To-do-list.py
@@ -1,5 +1,19 @@
-def Car(type):
-    model = str(type)+ " "+ "Honda";
-    return model
+class User:
+    def __init__(self):
+        self.name = Kidus
+        self.level = 0
+        
+    class To_do_list:
+        def __init__(self):
+            self.milestones = 1
+            self.task = 1
+            self.Frequency = 1
+            self.levels = 1
 
-print(Car(2007))
+    class milestones:
+        def __init__(self):
+            self.tasks
+            self.completionStatus
+            self.
+
+
